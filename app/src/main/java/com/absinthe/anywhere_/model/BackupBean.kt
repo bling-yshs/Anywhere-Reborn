3e35030f733cb28812bdde34e39f32463b84bf19
Cirrus CI <support@cirruslabs.org>
2026-08-14 19:31:33 +0800

    fix: preserve backup model in release

--- app/src/main/java/com/absinthe/anywhere_/model/BackupBean.kt
+++ app/src/main/java/com/absinthe/anywhere_/model/BackupBean.kt
@@ -1,9 +1,11 @@
 package com.absinthe.anywhere_.model
 
+import androidx.annotation.Keep
 import com.absinthe.anywhere_.model.database.AnywhereEntity
 import com.absinthe.anywhere_.model.database.PageEntity
 import com.google.gson.annotations.SerializedName
 
+@Keep
 data class BackupBean(
   @SerializedName("anywhereList") val anywhereList: List<AnywhereEntity>,
   @SerializedName("pageList") val pageList: List<PageEntity>
